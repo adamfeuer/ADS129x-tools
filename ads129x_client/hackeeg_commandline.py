d1da5eda16cad561227d22150c5374b1f2ae17d9
Adam Feuer <adam@adamfeuer.com>
2019-07-05 15:30:02 -0700

    autopep8 format

--- ads129x_client/hackeeg_commandline.py
+++ ads129x_client/hackeeg_commandline.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 
 import sys
-import hackeeg.driver
+from hackeeg.driver import HackEegBoard
 
-class HackEegDemo:
-   def __init__(self):
-      self.serialPortName = sys.argv[1]
-      pass
 
-   
-   def main(self):
-      self.hackeeg = hackeeg.driver.HackEegBoard(self.serialPortName)
-      self.hackeeg.setup()
-      while 1:
-         print "%s" % self.hackeeg.readSample()
+class HackEegCommandLine:
+    def __init__(self):
+        self.serialPortName = sys.argv[1]
+        pass
+
+    def main(self):
+        self.hackeeg = HackEegBoard(self.serialPortName)
+        self.hackeeg.setup()
+        while True:
+            sample = self.hackeeg.readSample()
+            print("{sample}")
+
 
 if __name__ == "__main__":
-   HackEegDemo().main()
+    HackEegCommandLine().main()
--- ads129x_client/hackeeg_demo.py
+++ ads129x_client/hackeeg_demo.py
@@ -3,17 +3,18 @@
 import sys
 import hackeeg.driver
 
-class HackEegDemo:
-   def __init__(self):
-      self.serialPortName = sys.argv[1]
-      pass
 
-   
-   def main(self):
-      self.hackeeg = hackeeg.driver.HackEegBoard(self.serialPortName)
-      self.hackeeg.setup()
-      while 1:
-         print "%s" % self.hackeeg.readSample()
+class HackEegDemo:
+    def __init__(self):
+        self.serialPortName = sys.argv[1]
+        pass
+
+    def main(self):
+        self.hackeeg = hackeeg.driver.HackEegBoard(self.serialPortName)
+        self.hackeeg.setup()
+        while True:
+            print "%s" % self.hackeeg.readSample()
+
 
 if __name__ == "__main__":
-   HackEegDemo().main()
+    HackEegDemo().main()
