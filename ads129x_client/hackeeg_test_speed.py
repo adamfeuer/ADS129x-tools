bc4beba391b22752541af51b838ba303b824884c
Adam Feuer <adam@adamfeuer.com>
2019-10-10 09:51:47 -0700

    more friendly commandline arguments; code cleanup

--- ads129x_client/hackeeg_test_speed.py
+++ ads129x_client/hackeeg_test_speed.py
@@ -1,39 +1,38 @@
 #!/usr/bin/env python
 
 import argparse
-import sys
 import time
 
 import hackeeg
 from hackeeg import ads1299
 from hackeeg.driver import SPEEDS, Status
 
-
 DEFAULT_NUMBER_OF_SAMPLES_TO_CAPTURE = 50000
 
 
+class HackEegTestApplicationException(Exception):
+    pass
+
+
 class HackEegTestApplication:
-    """Basic commandline test application – sets a test waveform on Channel 7, enters RDATAC (read data continuous) mode,
-       and outputs the samples received."""
+    """HackEEG speed test."""
+
     def __init__(self):
         self.serial_port_name = None
         self.hackeeg = None
         self.debug = False
         # self.debug = True
 
-    def setup(self, samples_per_second=500):
+    def setup(self, samples_per_second=8192):
         if samples_per_second not in SPEEDS.keys():
-            raise HackEegException("{} is not a valid speed; valid speeds are {}".format(
+            raise HackEegTestApplicationException("{} is not a valid speed; valid speeds are {}".format(
                 samples_per_second, sorted(SPEEDS.keys())))
         self.hackeeg.jsonlines_mode()
         self.hackeeg.sdatac()
         self.hackeeg.reset()
         self.hackeeg.blink_board_led()
         self.hackeeg.disable_all_channels()
-        # sample_mode = ads1299.HIGH_RES_250_SPS | ads1299.CONFIG1_const
-        sample_mode = ads1299.HIGH_RES_16k_SPS | ads1299.CONFIG1_const
-        # sample_mode = ads1299.HIGH_RES_8k_SPS | ads1299.CONFIG1_const
-        # sample_mode = ads1299.HIGH_RES_4k_SPS | ads1299.CONFIG1_const
+        sample_mode = SPEEDS[samples_per_second] | ads1299.CONFIG1_const
 
         self.hackeeg.wreg(ads1299.CONFIG1, sample_mode)
         test_signal_mode = ads1299.INT_TEST_4HZ | ads1299.CONFIG2_const
@@ -58,8 +57,11 @@ class HackEegTestApplication:
                             type=str)
         parser.add_argument("--debug", "-d", help="enable debugging output",
                             action="store_true")
-        parser.add_argument("--samples", "-s", help="how many samples to capture",
+        parser.add_argument("--samples", "-S", help="how many samples to capture",
                             default=DEFAULT_NUMBER_OF_SAMPLES_TO_CAPTURE, type=int)
+        parser.add_argument("--sps", "-s",
+                            help=f"ADS1299 samples per second setting- must be one of {sorted(list(SPEEDS.keys()))}",
+                            default=8192, type=int)
         args = parser.parse_args()
         if args.debug:
             self.debug = True
@@ -67,13 +69,15 @@ class HackEegTestApplication:
         self.serial_port_name = args.serial_port
         self.hackeeg = hackeeg.HackEEGBoard(self.serial_port_name, baudrate=2000000, debug=self.debug)
         self.hackeeg.connect()
-        self.setup()
+        self.setup(samples_per_second=args.sps)
         max_samples = args.samples
         start_time = time.perf_counter()
+        end_time = time.perf_counter()
         samples = 0
         while samples < max_samples:
             samples += 1
             result = self.hackeeg.read_rdatac_response()
+            end_time = time.perf_counter()
             if result:
                 status_code = result.get(self.hackeeg.MpStatusCodeKey)
                 data = result.get(self.hackeeg.MpDataKey)
@@ -88,7 +92,6 @@ class HackEegTestApplication:
             else:
                 print("no data to decode")
                 print(f"result: {result}")
-        end_time = time.perf_counter()
         duration = end_time - start_time
 
         self.hackeeg.send_command("stop")
@@ -101,7 +104,7 @@ class HackEegTestApplication:
 
         self.hackeeg.blink_board_led()
         print(f"duration in seconds: {duration}")
-        samples_per_second = max_samples/duration
+        samples_per_second = max_samples / duration
         print(f"samples per second: {samples_per_second}")
 
 
